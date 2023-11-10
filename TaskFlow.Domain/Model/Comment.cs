b73b5ff87a5b885a5bd497e2c9ab3070b12a195f
Przemysław Ciesielski <ciesiellski@gmail.com>
2023-11-10 20:48:03 +0100

    Add tables dependencies

--- TaskFlow.Domain/Model/Comment.cs
+++ TaskFlow.Domain/Model/Comment.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace TaskFlow.Domain.Model
 {
-    internal class Comment
+    public class Comment
     {
+        public int Id { get; set; }
+        public int TaskId { get; set; }
+        public string Content { get; set; }
+        public int CreatedById { get; set; }
+        public int UpdatedById { get; set; }
+        public DateTime CreatedDate { get; set;}
+        public DateTime UpdatedDate { get; set;}
+        public bool IsPublic { get; set; } = true;
+        //1:n
+        public Task Task { get; set; }
     }
 }
--- TaskFlow.Domain/Model/Task.cs
+++ TaskFlow.Domain/Model/Task.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace TaskFlow.Domain.Model
 {
-    internal class Task
+    public class Task
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StatusId { get; set; }
+        public int CategoryId { get; set; }
+        public int PriorityId { get; set; }
+        public bool IsPublic { get; set; } = true;
+        public int ProjectId { get; set; }
+        public int AssignedToId { get; set; }
+        
+        //1:1
+        public TaskAuditableModel TaskAuditableModel  { get; set; }
+        //1:n
+        public virtual Project Project { get; set; }
+        //1:n
+        public Status Status { get; set; }
+        //1:n
+        public Category Category { get; set; }
+        //1:n
+        public Priority Priority { get; set; }
+        //1:n
+        public ICollection<Comment> Comments { get; set; }
+        //m:n (1:n)
+        public ICollection<TaskTag> TaskTag { get; set; }
+
     }
 }
