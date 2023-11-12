4e8ffcde34e477c7b63bc0ac4eebad1e0c6a9c75
Przemysław Ciesielski <ciesiellski@gmail.com>
2023-11-12 22:51:04 +0100

    Add task repository methods

--- TaskFlow.Infrastructure/Repositories/ProjectRepository.cs
+++ TaskFlow.Infrastructure/Repositories/ProjectRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace TaskFlow.Infrastructure.Repositories
 {
-    internal class ProjectRepository
+    public class ProjectRepository
     {
+        private readonly DbContext _dbContext;
+        public ProjectRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
     }
 }
--- TaskFlow.Infrastructure/Repositories/TaskRepository.cs
+++ TaskFlow.Infrastructure/Repositories/TaskRepository.cs
@@ -2,11 +2,87 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Task = TaskFlow.Domain.Model.Task;
 
 namespace TaskFlow.Infrastructure.Repositories
 {
-    internal class TaskRepository
+    public class TaskRepository
     {
+        private readonly DbContext _dbContext;
+        public TaskRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int Add(Task task)
+        {
+            _dbContext.Tasks.Add(task);
+            _dbContext.SaveChanges();
+            return task.Id;
+        }
+
+        public void Delete(int taskId) 
+        {
+            var task = _dbContext.Tasks.Find(taskId);
+            if(task != null)
+            {
+                _dbContext.Tasks.Remove(task);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public IQueryable<Task> GetAll()
+        {
+            return _dbContext.Tasks;
+        }
+
+        public Task GetTaskById(int taskId) 
+        {
+            var task = _dbContext.Tasks.FirstOrDefault(t => t.Id == taskId);
+            return task;
+        }
+
+        public IQueryable<Task> GetTaskByCategoryId(int categoryId)
+        {
+            var tasks = _dbContext.Tasks.Where(t=>t.CategoryId == categoryId);
+            return tasks;
+        }
+
+        public IQueryable<Task> GetTasksByStatusId(int statusId)
+        {
+            var tasks = _dbContext.Tasks.Where(t=>t.StatusId == statusId);
+            return tasks;
+        }
+
+        public IQueryable<Task> GetTasksByPriorityId(int priorityId)
+        {
+            var tasks = _dbContext.Tasks.Where(t=>t.ProjectId == priorityId);
+            return tasks;
+        }
+
+        public IQueryable<Task> GetTaskByAssignedToId(int assignedToId)
+        {
+            var tasks = _dbContext.Tasks.Where(t=>t.AssignedToId == assignedToId);
+            return tasks;
+        }
+
+        public IQueryable<Task> GetTaskByProjectId(int projectId)
+        {
+            var tasks = _dbContext.Tasks.Where(t=>t.ProjectId != projectId);
+            return tasks;
+        }
+
+        public IQueryable<Task> GetPublicOrPrivateTasks(bool isPublic)
+        {
+            if(isPublic)
+            {
+                var tasks = _dbContext.Tasks.Where(t => t.IsPublic == true);
+                return tasks;
+            }else
+            {
+                var tasks = _dbContext.Tasks.Where(t => t.IsPublic == false);
+                return tasks;
+            }
+        }
     }
 }
