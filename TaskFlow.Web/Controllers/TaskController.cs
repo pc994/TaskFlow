f23b2b2cf713adb5892b4a99527f67cca66746df
Przemysław Ciesielski <ciesiellski@gmail.com>
2023-11-14 14:42:35 +0100

    add viewmodels

---
+++ TaskFlow.Web/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
